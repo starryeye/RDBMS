0d549fd448b782490db6d998d69fb58429a538f2
starryeye <see2818@naver.com>
2025-12-31 21:24:52 +0900

    [starryeye, 2025.12.31] - rdbms
    내용
    - rdbms

--- sql/sub2_dml/example_1/select/sub9_aggregation_and_grouping/sub4_group_by_2.sql
+++ sql/sub2_dml/example_1/select/sub9_aggregation_and_grouping/sub4_group_by_2.sql
@@ -26,11 +26,31 @@ GROUP BY
 
 
 
--- todo
---  아래 쿼리의 의미.. 암묵적 group by
---  최저가 최고가에 해당하는 customer_name 도 같이 출력하고 싶다면..
+-- 4. 암묵적 GROUP BY
+-- 아래 쿼리에는 GROUP BY 절이 없지만, SELECT 절에 집계함수만 존재하여
+-- DB 는 전체 테이블 row 를 하나의 그룹(row)로 묶는다.
 SELECT
-    MIN(price) AS 최저가,
-    MAX(price) AS 최고가
+    MIN(price) AS `주문 최저가`,
+    MAX(price) AS `주문 최고가`
 FROM
     order_stat;
+
+
+-- 참고
+-- 최저가 최고가에 주문한 customer_name 도 같이 출력하고 싶어서..
+-- 아래와 같은 쿼리를 작성하면 에러가 발생한다.
+--      우선, 아래는 GROUP BY 가 없지만, 암묵적 GROUP BY 가 적용되어 전체 테이블 row 를 하나의 row 로 묶는다.
+--      따라서, customer_name 을 출력할 때는 어떤 값으로 대표할 지 결정 할 수가 없다.
+-- 참고
+-- 여기서 주문 최저가/최고가에 해당하는 customer_name 을 사용하면 되지 않냐라고 단순하게 생각 할 수 있는데.. 안된다...
+-- 왜냐하면, 그룹 대상은 전체 테이블로 암묵적으로 되었지.. 주문 최저가/최고가 의 row 하나가 지정된게 아니기 때문이다.
+--      또한, 주문 최저가/최고가가 하나의 row 로 나오지.. 두개의 row 로 나오지 않는다. 따라서 각각의 product_name 이 동일함이 보장되지도 않는다.
+--      그렇다고해서.. customer_name 과 MIN(price) 두개의 컬럼만 조회하더라도 그룹 대상은 여전히 전체 테이블이므로 customer_name 의 대표 값은 DB 가 알지 못한다.
+-- 그래서.. 서브 쿼리를 사용하거나, 최저가/최고가 각각 조회한다면 order by 를 사용해야한다.
+SELECT
+    customer_name,
+    MIN(price) AS `주문 최저가`,
+    MAX(price) AS `주문 최고가`
+FROM
+    order_stat;
+
