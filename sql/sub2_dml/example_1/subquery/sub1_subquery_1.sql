24c2ed84dc296ae204b1c76e81188ebc71dfa27a
starryeye <see2818@naver.com>
2026-01-08 21:00:03 +0900

    [starryeye, 2026.01.08] - rdbms
    내용
    - rdbms

---
+++ sql/sub2_dml/example_1/subquery/sub1_subquery_1.sql
@@ -0,0 +1,11 @@
+-- datasource: my-rdbms-3
+-- precondition:
+--      sql.sub2_dml.example_1.join.sub1_precondition.sql 실행
+
+-- 테이블간 연관관계
+-- users (1) -----< orders (N) >----- products (1)
+-- employees (1) -----< employees (N)
+
+
+
+-- 서브쿼리 결과가 단일컬럼, 단일행인 경우에 대해 알아본다. (스칼라 서브쿼리)
